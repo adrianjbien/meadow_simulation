3333d048887eb7571ed06f0d7f2178eec8423f7c
247630 Bień Adrian <247630@edu.p.lodz.pl>
2024-11-17 16:52:15 +0100

    wolf changes p.2

--- Wolf.py
+++ Wolf.py
@@ -7,8 +7,10 @@ class Wolf(Animal):
         init_y = 0.0
         super().__init__(init_x, init_y, step_length)
 
-    def sheep_to_wolf_distance(self, sheep):
-        return ((self.get_x_cord - sheep.get_x_cord()) ** 2 + (self.get_y_cord - sheep.get_y_cord()) ** 2) ** 0.5
+
+    def sheep_to_wolf_distance(self, sh):
+        return ((self.get_x_cord - sh.get_x_cord()) ** 2 + (self.get_y_cord - sh.get_y_cord()) ** 2) ** 0.5
+
 
     def find_closest_sheep(self, sheep):
         closest = sheep[0]
@@ -17,10 +19,14 @@ class Wolf(Animal):
                 closest = sh
         return closest
 
-    def is_sheep_in_range(self):
-        cos = 0
 
-    def move(self):
+    def is_sheep_in_range(self, closest_sheep):
+        if self.sheep_to_wolf_distance(closest_sheep) <= self.step_length:
+            closest_sheep.set_alive = False
+            # dalej
+
+
+    def move(self, sheep=None):  # dodanie parametru mimo ze nie ma go w metodzie klasy bazowej,
         self.position.x = 2
         self.position.y = 2
         # implementacja kroku i sprawdzanie ktora owca jest najblizej i czy jest w zasiegu
